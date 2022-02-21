31363810a5c8aabfb865375a102cceb88c0c31f1
rajan <rajanlama79@gmail.com>
2022-02-21 15:41:24 +0545

    tour react

---
+++ src/Tour.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Tour = ({id,image,name,info,price,removeTour}) => {
+   const[readMore,setReadMore]=useState(false);
+  return (<article>
+    <img src={image} alt={name}/>
+    <footer>
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+       <p>{readMore ? info : `${info.substring(0,200)} ...`} 
+       <button  onClick={()=>setReadMore(!readMore)}>{readMore ? 'show less' :'read more'}</button>
+       </p>
+       <button onClick={()=>removeTour(id)}>Not intrested</button>
+    </footer>
+
+  </article>);
+};
+
+export default Tour;
